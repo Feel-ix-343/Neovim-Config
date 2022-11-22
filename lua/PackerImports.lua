c57c43041f046aae36b52fcddaa53053d00d2d85
Felix Zeller <felixazeller@gmail.com>
2022-11-22 08:02:32 -0500

    Better lua completion

--- lua/PackerImports.lua
+++ lua/PackerImports.lua
@@ -2,6 +2,7 @@ local use = require('packer').use
 require('packer').startup(function()
 	use 'wbthomason/packer.nvim' -- Package manager
 	use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+  use 'folke/neodev.nvim'
   use { "williamboman/mason.nvim" }
   use 'williamboman/mason-lspconfig.nvim'
   use {
